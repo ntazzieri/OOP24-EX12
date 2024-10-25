ad22fcea50b79f8c9f9e4f03378cf5a6d06cc251
Nicolas Tazzieri <nicolas.tazzieri@studio.unibo.it>
2024-10-25 21:40:51 +0200

    Implemented availableQueues(), openNewQueue(), isQueueEmpty() in p12.exercise.MultiQueueImpl

--- src/p12/exercise/MultiQueueImpl.java
+++ src/p12/exercise/MultiQueueImpl.java
@@ -1,27 +1,36 @@
 package p12.exercise;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class MultiQueueImpl<T, Q> implements MultiQueue<T, Q>{
 
+    private final Map<Q, Queue<T>> queues = new HashMap<>();
+
     @Override
     public Set<Q> availableQueues() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'availableQueues'");
+        return new HashSet<Q>(queues.keySet());
     }
 
     @Override
-    public void openNewQueue(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'openNewQueue'");
+    public void openNewQueue(final Q queue) {
+        if(queues.containsKey(queue)) {
+            throw new IllegalArgumentException();
+        }
+        queues.put(queue, new LinkedList<T>());
     }
 
     @Override
     public boolean isQueueEmpty(Q queue) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'isQueueEmpty'");
+        if(!queues.containsKey(queue)) {
+            throw new IllegalArgumentException();
+        }
+        return queues.containsKey(queue);
     }
 
     @Override
